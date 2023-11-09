425b390e9ef826ad77090d591937cbc9a08da66f
Wolltoffel <lzandianpour@stud.macromedia.de>
2023-11-09 01:06:58 +0100

    Fixed Fog Script

--- 2_UnityProject/Assets/2_Game/1_Gas/2_Volumetric/VolumetricFogHandler.cs
+++ 2_UnityProject/Assets/2_Game/1_Gas/2_Volumetric/VolumetricFogHandler.cs
@@ -3,51 +3,107 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
-using UnityEngine.VFX;
 
 public class VolumetricFogHandler : MonoBehaviour
 {   
     [SerializeField] LayerMask layerMask;
-    Vector4[] sphereInfo;
+    List<Transform> intersectSmokeTransforms  = new List<Transform>();
     int maxMasks = 3;
 
     LocalVolumetricFog localVolumetricFog;
 
     void Start()
-    {
-        ImportSmokeMasks();
+    {   
         localVolumetricFog = GetComponent<LocalVolumetricFog>();
+        ResetSmokeMasks();
     }
 
-    void Update()
+    void LateUpdate()
     {
-        SetSpheres(sphereInfo);
+        UpdateSpheres(intersectSmokeTransforms.ToArray());
+        CheckColliders();
     }
 
-    void SetSpheres(Vector4[] sphereInfoInput)
+    void UpdateSpheres(Transform [] transforms) 
     {
-        for (int i =0;i<maxMasks;i++)
+        List<Vector4> sphereInfos  = new List<Vector4>();
+        foreach(Transform transformItem in transforms)
         {
-                float circleRadius  = localVolumetricFog.parameters.materialMask.GetVector($"_Sphere0{i+1}").w;
-                localVolumetricFog.parameters.materialMask.SetVector($"_Sphere0{i+1}",sphereInfoInput[i]);       
+            sphereInfos.Add(VectorHelper.Convert3To4(transformItem.position,2));
+        }
+        SetSpheres(sphereInfos.ToArray());
+    }
+
+    void SetSpheres(Vector4[] sphereInfos)
+    {
+        for (int i =0;i<sphereInfos.Length;i++)
+        {   
+            localVolumetricFog.parameters.materialMask.SetVector($"_Sphere0{i+1}", sphereInfos[i]);   
         }
     }
 
-    void ImportSmokeMasks()
+    void AddSmokeMask(Transform transform)
     {
-        sphereInfo = new Vector4[maxMasks];
-        IEnumerable <IIntersectSmoke> findObjects = FindObjectsOfType<MonoBehaviour>().OfType<IIntersectSmoke>();
-        List<IIntersectSmoke> allObjects = findObjects.ToList<IIntersectSmoke>();
-        int counter = 0;
-        for (int i = 0; i < allObjects.Count(); i++)
-        {
-                if (counter>=maxMasks)
-                {
-                    Debug.Log("There are more objects that mask out smoked then previsouly set");
-                    break;
-                }     
-                sphereInfo[counter] = allObjects[i].GetSphereInformation();
-                counter++;
-        }
+        if (intersectSmokeTransforms.Count<maxMasks)
+            intersectSmokeTransforms.Add(transform);
+        else
+            Debug.Log("There are more objects that mask out smoked then previsouly set up");
     }
+
+    void DeleteSmokeMask(Transform transform)
+    {
+        intersectSmokeTransforms.Remove(transform);
+    }
+
+    void ResetSmokeMasks()
+    {
+        intersectSmokeTransforms.Clear();
+        ResetSpheres();
+    }
+
+    void ResetSpheres()
+    {
+        //Reset Sphere Values in Material
+        Vector4[] emptySpheres = new Vector4[maxMasks];
+        for (int i = 0; i < maxMasks; i++)
+        {
+            emptySpheres[i] = Vector4.zero;
+        }
+        SetSpheres(emptySpheres);
+    }
+
+    void CheckColliders()
+    {   
+        Vector3 colliderSize = localVolumetricFog.parameters.size;
+        Collider[] hitColliders = Physics.OverlapBox(transform.position, colliderSize/2, Quaternion.identity, layerMask);
+
+        //Check if object is already in list
+        for (int i = 0; i < hitColliders.Length; i++)
+        {   
+            Debug.Log (hitColliders[i].gameObject.name);
+            if (!intersectSmokeTransforms.Contains(hitColliders[i].transform))
+            {
+                AddSmokeMask(hitColliders[i].transform);
+            }
+        }
+
+        //Check if object in list moved out
+        List<Transform> hitColliderTransforms = new List<Transform>();
+        foreach(Collider hitCollider in hitColliders)
+        {
+            hitColliderTransforms.Add(hitCollider.transform);
+        }
+
+        for (int i = 0; i < intersectSmokeTransforms.Count; i++)
+        {   
+            if (!hitColliderTransforms.Contains(intersectSmokeTransforms[i]))
+            {
+                DeleteSmokeMask(intersectSmokeTransforms[i]);
+            }
+        }
+
+        ResetSpheres();
+        UpdateSpheres(intersectSmokeTransforms.ToArray());
+    }
+
 }
