df1bf1af4ac8af4fa1569a7103d1088660f3c61d
Toffeefee8 <gibbon-preamp0x@icloud.com>
2023-11-22 13:04:31 +0100

    Update RoomFadeLogic.cs

--- 2_UnityProject/Assets/2_Game/RoomBlock/RoomFadeLogic.cs
+++ 2_UnityProject/Assets/2_Game/RoomBlock/RoomFadeLogic.cs
@@ -4,41 +4,66 @@ using UnityEngine;
 
 public class RoomFadeLogic : MonoBehaviour
 {
-    [SerializeField] private Material material;
+    [Header("Parameters")]
+    [SerializeField] private Material[] materials;
     [SerializeField] private int playerLayer = 9;
     [SerializeField] private float maxFadeRadius = 50;
     [SerializeField] private float fadeTime = 1;
 
     private Coroutine fadeRoutine;
 
+    //Shader Variable Names
+    private string nameRadius = "_Radius";
+    private string nameShouldFade = "_ShouldFade";
+    private string nameEpicenter = "_Epicenter";
+
     void Start()
     {
-        if (material == null)
+        if (materials == null || materials.Length == 0)
         {
-            material = GetComponentInChildren<Renderer>().material;
-            if (material == null)
-            {
-                Debug.LogWarning("Something went wrong. No material found.");
-            }
+            Debug.LogWarning("Something went wrong. No material assigned.");
         }
-
-        SetVisible(false);
+        else
+        {
+            SetVisible(false);
+        }
     }
 
     private void SetVisible(bool visible)
     {
-        material.SetInt("_ShouldFade", visible ? 0 : 1);
-        material.SetFloat("_Radius", visible ? maxFadeRadius : 0);
+        SetMaterialInt(nameShouldFade, visible ? 0 : 1);
+        SetMaterialFloat(nameRadius, visible ? maxFadeRadius : 0);
     }
 
+    /*
+    private Material[] GetAllMaterialsInChildren(string targetShaderName)
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        List<Material> materials = new List<Material>();
+        List<string> materialsAdded = new List<string>();
+
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            if (renderers[i].material.shader.name == targetShaderName)
+            {
+                if (!materialsAdded.Contains(renderers[i].sharedMaterial.name))
+                {
+                    materialsAdded.Add(renderers[i].sharedMaterial.name);
+                    materials.Add(renderers[i].sharedMaterials[0]);
+                }
+            }
+        }
+
+        return materials.ToArray();
+    }
+    */
+
+    #region Triggers
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Entered");
-
         if (other.gameObject.layer == playerLayer)
         {
-            Debug.Log("CorrectLayer");
-            material.SetVector("_Epicenter", VectorHelper.Convert3To2(other.transform.position));
+            SetMaterialVector(nameEpicenter, VectorHelper.Convert3To2(other.transform.position));
             StartFade(true);
         }
     }
@@ -47,11 +72,13 @@ public class RoomFadeLogic : MonoBehaviour
     {
         if (other.gameObject.layer == playerLayer)
         {
-            material.SetVector("_Epicenter", VectorHelper.Convert3To2(other.transform.position));
+            SetMaterialVector(nameEpicenter, VectorHelper.Convert3To2(other.transform.position));
             StartFade(false);
         }
     }
+    #endregion
 
+    #region Fade Logic
     private void StartFade(bool bFadeIn)
     {
         //Stop Prior Fade
@@ -66,19 +93,20 @@ public class RoomFadeLogic : MonoBehaviour
 
     private IEnumerator Fade(bool bFadeIn)
     {
-        //Get/Set Targets
-        float currentRadius = material.GetFloat("_Radius");
+        //Get - Set Targets
+        float currentRadius = materials[0].GetFloat(nameRadius);
         float targetRadius = bFadeIn ? maxFadeRadius : 0;
+        SetMaterialFloat(nameRadius, currentRadius);
 
         //Make Fadable
-        material.SetInt("_ShouldFade", 1);
+        SetMaterialInt(nameShouldFade, 1);
 
         //Set Radius Over Time
         while (bFadeIn ? currentRadius < targetRadius : targetRadius < currentRadius)
         {
             float addedDeltaTime = bFadeIn ? Time.deltaTime : -Time.deltaTime;
             currentRadius += addedDeltaTime * maxFadeRadius / fadeTime;
-            material.SetFloat("_Radius", currentRadius);
+            SetMaterialFloat(nameRadius, currentRadius);
             yield return null;
         }
 
@@ -87,4 +115,29 @@ public class RoomFadeLogic : MonoBehaviour
 
         fadeRoutine = null;
     }
+    #endregion
+
+    #region Set Material Values
+    private void SetMaterialInt(string intName, int value)
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].SetInt(intName, value);
+        }
+    }
+    private void SetMaterialFloat(string floatName, float value)
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].SetFloat(floatName, value);
+        }
+    }
+    private void SetMaterialVector(string vectorName, Vector4 value)
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].SetVector(vectorName, value);
+        }
+    }
+    #endregion
 }
