07c6681fac96ec8efc179fe86e5f448d7b3b208a
Toffeefee8 <gibbon-preamp0x@icloud.com>
2023-12-22 23:53:34 +0100

    Temporary Voice Trigger Fix

--- 2_UnityProject/Assets/1_Game/3_Level/1_Interactables/9_VoiceTrigger/VoiceTrigger.cs
+++ 2_UnityProject/Assets/1_Game/3_Level/1_Interactables/9_VoiceTrigger/VoiceTrigger.cs
@@ -1,65 +1,66 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
-public enum CharacterMode
-{
-    Both,Active, Inactive
-}
-
 
 [CustomEditor(typeof(VoiceTrigger))]
 public class MyScriptEditor : Editor
 {
-  public override void OnInspectorGUI()
-  {
-    var voiceTrigger = target as VoiceTrigger;
+    private const string PlayOnceKey = "VoiceTrigger_PlayOnce";
+    private const string PlayOnceBeforeRandomKey = "VoiceTrigger_PlayOnceBeforeRandom";
+    private const string VoiceLineKey = "VoiceTrigger_VoiceLine";
 
-    EditorGUILayout.LabelField("Trigger Conditions", EditorStyles.boldLabel);
-
-    voiceTrigger.playOnce = EditorGUILayout.Toggle("Play Once", voiceTrigger.playOnce);
-    
-    //Gender Specific Voices
-    Interactable interactable = voiceTrigger.GetComponent<Interactable>();
-    if (interactable && interactable.specificCharacterAccess==CharacterType.None)
+    public override void OnInspectorGUI()
     {
-        voiceTrigger.characterType = (CharacterType)EditorGUILayout.EnumPopup("Character",voiceTrigger.characterType);
+        serializedObject.Update();
+
+        var voiceTrigger = (VoiceTrigger)target;
+
+        voiceTrigger.playOnce = EditorPrefs.GetBool(PlayOnceKey, voiceTrigger.playOnce);
+        voiceTrigger.playOnceBeforeRandom = EditorPrefs.GetBool(PlayOnceBeforeRandomKey, voiceTrigger.playOnceBeforeRandom);
+        voiceTrigger.voiceLine = (E_1_Voicelines)EditorPrefs.GetInt(VoiceLineKey, (int)voiceTrigger.voiceLine);
+
+        voiceTrigger.playOnce = EditorGUILayout.Toggle("Play Once", voiceTrigger.playOnce);
+
+        voiceTrigger.randomizeVoicelines = EditorGUILayout.Toggle("Randomize Voicelines", voiceTrigger.randomizeVoicelines);
+
+        //Gender Specific Voices
+        Interactable interactable = voiceTrigger.GetComponent<Interactable>();
+        if (interactable && interactable.specificCharacterAccess==CharacterType.None)
+        {
+            voiceTrigger.characterType = (CharacterType)EditorGUILayout.EnumPopup("Character",voiceTrigger.characterType);
+        }
+        else
+        {
+            voiceTrigger.characterType = CharacterType.None;
+        }
+
+        voiceTrigger.extraWaitTimeAfterClip = EditorGUILayout.FloatField("Wait Time Between Clips", voiceTrigger.extraWaitTimeAfterClip);
+
+        voiceTrigger.triggerType = (TriggerType)EditorGUILayout.EnumPopup("TriggerType",voiceTrigger.triggerType);
+
+        if (voiceTrigger.randomizeVoicelines)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("randomVoicelines"),true);
+            voiceTrigger.playOnceBeforeRandom = EditorGUILayout.Toggle("Play Once Before Random", voiceTrigger.playOnceBeforeRandom);
+        }    
+        else
+            voiceTrigger.voiceLine = (E_1_Voicelines)EditorGUILayout.EnumPopup("Voiceline",voiceTrigger.voiceLine);
+
+        if (voiceTrigger.playOnceBeforeRandom)
+            voiceTrigger.voiceLine = (E_1_Voicelines)EditorGUILayout.EnumPopup("Voiceline before Random",voiceTrigger.voiceLine);
+
+        EditorPrefs.SetBool(PlayOnceKey, voiceTrigger.playOnce);
+        EditorPrefs.SetBool(PlayOnceBeforeRandomKey, voiceTrigger.playOnceBeforeRandom);
+        EditorPrefs.SetInt(VoiceLineKey, (int)voiceTrigger.voiceLine);
+
+        serializedObject.ApplyModifiedProperties();
     }
-    else
-    {
-        voiceTrigger.characterType = CharacterType.None;
-    }
-
-    voiceTrigger.triggerType = (TriggerType)EditorGUILayout.EnumPopup("TriggerType",voiceTrigger.triggerType);
-
-    voiceTrigger.characterMode = (CharacterMode)EditorGUILayout.EnumPopup("Required Character Mode",voiceTrigger.characterMode);
-
-    EditorGUILayout.Space(10);
-    EditorGUILayout.LabelField("Voiceline Settings", EditorStyles.boldLabel);
-
-    voiceTrigger.extraWaitTimeAfterClip = EditorGUILayout.FloatField("Wait Time Between Clips", voiceTrigger.extraWaitTimeAfterClip);
-
-    voiceTrigger.randomizeVoicelines = EditorGUILayout.Toggle("Randomize Voicelines", voiceTrigger.randomizeVoicelines);
-
-    if (voiceTrigger.randomizeVoicelines)
-    {
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("randomVoicelines"),true);
-        voiceTrigger.playOnceBeforeRandom = EditorGUILayout.Toggle("Play Once Before Random", voiceTrigger.playOnceBeforeRandom);
-    }    
-    else
-        voiceTrigger.voiceLine = (E_1_Voicelines)EditorGUILayout.EnumPopup("Voiceline",voiceTrigger.voiceLine);
-
-    if (voiceTrigger.playOnceBeforeRandom)
-        voiceTrigger.voiceLine = (E_1_Voicelines)EditorGUILayout.EnumPopup("Voiceline before Random",voiceTrigger.voiceLine);
-
-    serializedObject.ApplyModifiedProperties();
-
-
-  }
 }
 
 
@@ -74,48 +75,46 @@ public class VoiceTrigger : MonoBehaviour
     int lastRandom = 500;
 
     static List<VoiceTrigger>otherVoiceTriggers = new List<VoiceTrigger>();
+
     public Coroutine coroutine;
     public float extraWaitTimeAfterClip = 1f;
-    public bool playOnce = false;
+    public bool playOnce;
     public bool playOnceBeforeRandom;
     public E_1_Voicelines voiceLine;
     public bool randomizeVoicelines;
     public E_1_Voicelines[] randomVoicelines;
     public TriggerType triggerType;
+
     public CharacterType characterType = CharacterType.None;
-    public CharacterMode characterMode = CharacterMode.Both;
 
+    private void Awake()
+    {
+        Debug.Log("Awake - playOnce: " + playOnce + ", playOnceBeforeRandom: " + playOnceBeforeRandom + ", voiceLine: " + voiceLine);
+    }
 
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable - playOnce: " + playOnce + ", playOnceBeforeRandom: " + playOnceBeforeRandom + ", voiceLine: " + voiceLine);
+    }
+
+    // Start is called before the first frame update
     void Start()
     {
         interactable = GetComponent<Interactable>();
 
         if (triggerType== TriggerType.OnTrigger)
         {
-
-            interactable.triggerEvent+=LoadVoiceLine;
+            interactable.triggerEvent += LoadVoiceLine;
 
             if (!playOnce)
-                interactable.untriggerEvent+=Untrigger;
+                interactable.untriggerEvent += Untrigger;
         }
         else if (triggerType == TriggerType.OnHighlight)
         {
-            if (characterMode != CharacterMode.Inactive)
-            {
-                interactable.highlightEvent+=LoadVoiceLine;
-            
-                if (!playOnce)
-                    interactable.unhiglightEvent+=Untrigger;
-            }
-            else
-            {
-                interactable.aiEnterEvent+=LoadVoiceLine;
-                interactable.aiStayEvent += LoadVoiceLine;
-                
-                if (!playOnce)
-                    interactable.aiExitEvent+=Untrigger;
-            }   
+            interactable.highlightEvent += LoadVoiceLine;
 
+            if (!playOnce)
+                interactable.unhiglightEvent += Untrigger;
         }
 
         AddOtherVoiceTriggerRange(GetComponents<VoiceTrigger>());
@@ -126,8 +125,7 @@ public class VoiceTrigger : MonoBehaviour
     {
         if (coroutine==null && !triggered 
         && CheckCharacterTypes(movement)
-        && CheckOtherVoicelines()
-        && CheckCharacterMode(movement))
+        && CheckOtherVoicelines())
         {
             triggered = true;
 
@@ -139,11 +137,12 @@ public class VoiceTrigger : MonoBehaviour
                 voicelineToPlay = voiceLine;
 
             if (playOnceBeforeRandom)
-            {   
+            {
                 playOnceBeforeRandom = false;
                 voicelineToPlay = voiceLine;
-            }    
+            }
 
+            Debug.Log(randomVoicelines.Length);
             string fileName = Enum.GetName(typeof(E_1_Voicelines),voicelineToPlay);
             fileName = RemoveFirstUnderscore(fileName);
             AsyncOperationHandle<AudioClip> asyncOperationHandle =  Addressables.LoadAssetAsync<AudioClip>("Assets/4_Assets/2_Sound/1_Voicelines/"+fileName+".wav");
@@ -168,7 +167,7 @@ public class VoiceTrigger : MonoBehaviour
     E_1_Voicelines RandomVoiceLine()
     {
         int random = UnityEngine.Random.Range(0,randomVoicelines.Length-1);
-        if(random==lastRandom)
+        if (random==lastRandom)
             random = (random + 1) % randomVoicelines.Length;
         lastRandom = random;
         return randomVoicelines[random];
@@ -218,26 +217,6 @@ public class VoiceTrigger : MonoBehaviour
         return interactable.specificCharacterAccess == CharacterType.None && movement.characterType == characterType && characterType!=CharacterType.None
          ||interactable.specificCharacterAccess!= CharacterType.None
          ||characterType==CharacterType.None;
-    }
-
-    bool CheckCharacterMode(Movement movement)
-    {
-        Movement activeMovement = CharacterManager.ActiveCharacterData.movement;
-
-        switch(characterMode)
-        {
-            case CharacterMode.Both:
-                return true;
-            case CharacterMode.Active:
-                if (movement == activeMovement)
-                    return true;
-                break;
-            case CharacterMode.Inactive:
-                if (movement != activeMovement)
-                    return true;
-                break;
-        }
-        return false;
     }
 
     void AddOtherVoiceTriggerRange(VoiceTrigger[] voiceTriggers)
