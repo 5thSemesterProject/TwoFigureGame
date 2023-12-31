28123d95ff996e9306b174123359bfd3bfe995fa
Toffeefee8 <gibbon-preamp0x@icloud.com>
2023-12-31 12:57:06 +0100

    Constraint Loader Update

--- 2_UnityProject/Assets/2_Resources/1_ParentAssets/2_Character/ContraintLoader.cs
+++ 2_UnityProject/Assets/2_Resources/1_ParentAssets/2_Character/ContraintLoader.cs
@@ -4,31 +4,71 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
-[ExecuteInEditMode]
+enum HeadState
+{
+    Idle,
+    Move,
+    Off,
+}
+
 [RequireComponent(typeof(MultiAimConstraint))]
 public class ContraintLoader : MonoBehaviour
 {
+    public bool overrideOnlyIdle = true;
+
     [SerializeField] private string headToFind;
     [SerializeField] private float radius = 5;
-    [SerializeField] private AnimationCurve weightCurve;
+    [SerializeField] private AnimationCurve weightCurveIdle;
+    [SerializeField] private AnimationCurve weightCurveMove;
     [SerializeField] private bool debug;
     private MultiAimConstraint constraint;
     private Transform target;
+    private Movement movement;
 
+    #region Startup
+    //Load values on Editor Change
     private void OnValidate()
     {
         LoadConstraint();
+        movement = GetComponentInParent<Movement>();
     }
 
     private void Awake()
     {
         LoadConstraint();
+        movement = GetComponentInParent<Movement>();
+    }
+    #endregion
+
+    private HeadState CheckState(Movement movement)
+    {
+        if (movement == null)
+            return HeadState.Idle;
+
+        CharacterData data;
+        if (movement.characterType == CharacterType.Man)
+            data = CharacterManager.manData;
+        else
+            data = CharacterManager.womanData;
+
+        if (data == null)
+            return HeadState.Idle;
+
+        if (data.currentState.GetType() == typeof(IdleState))
+            return HeadState.Idle;
+
+        if (data.currentState.GetType() == typeof(MoveState))
+            return HeadState.Move;
+
+        return HeadState.Off;
     }
 
     private void LoadConstraint()
     {
+        //Get constraint and target
         constraint = GetComponent<MultiAimConstraint>();
         GameObject targetGameobject = GameObject.Find(headToFind);
+
         if (targetGameobject != null)
         {
             target = targetGameobject.transform;
@@ -44,8 +84,28 @@ public class ContraintLoader : MonoBehaviour
 
     private void Update()
     {
-        float distance = Vector3.Distance(transform.position, target.position);
-        constraint.weight = Mathf.Clamp01(weightCurve.Evaluate(Mathf.Clamp01(distance / radius)));
+        if (overrideOnlyIdle)
+        {
+            float distance = Vector3.Distance(transform.position, target.position);
+            constraint.weight = Mathf.Clamp01(weightCurveIdle.Evaluate(Mathf.Clamp01(distance / radius)));
+            return;
+        }
+
+        switch (CheckState(movement))
+        {
+            case HeadState.Idle:
+                float distance = Vector3.Distance(transform.position, target.position);
+                constraint.weight = Mathf.Clamp01(weightCurveIdle.Evaluate(Mathf.Clamp01(distance / radius)));
+                return;
+            case HeadState.Move:
+                distance = Vector3.Distance(transform.position, target.position);
+                constraint.weight = Mathf.Clamp01(weightCurveMove.Evaluate(Mathf.Clamp01(distance / radius)));
+                return;
+            case HeadState.Off:
+                break;
+        }
+
+        constraint.weight = 0;
     }
 
     private void OnDrawGizmos()
