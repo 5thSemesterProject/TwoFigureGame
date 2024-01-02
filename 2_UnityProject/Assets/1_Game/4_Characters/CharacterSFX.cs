48cb71b12e91579d6f0fbc59a08d4249362096a7
Wolltoffel <lzandianpour@stud.macromedia.de>
2024-01-02 23:33:07 +0100

    Basic Setup For Character SFX

--- 2_UnityProject/Assets/1_Game/4_Characters/CharacterSFX.cs
+++ 2_UnityProject/Assets/1_Game/4_Characters/CharacterSFX.cs
@@ -1,12 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
+[System.Serializable]
+class FootstepSound
+{
+    public string tag;
+    public E_5_Character[] sounds;
+    public bool setAsDefault;
+    int lastRandom;
+
+    public E_5_Character GetRandomSound()
+    {
+        int random = AudioUtility.RandomNumber(lastRandom,sounds.Length,out lastRandom);
+        return sounds[random];
+    }
+}
+
+
 public class CharacterSFX : MonoBehaviour
 {
+    [SerializeField]FootstepSound[] footstepSounds;
     List<AudioClip> audioClips = new List<AudioClip>();
 
     void  Awake()
@@ -28,6 +46,49 @@ public class CharacterSFX : MonoBehaviour
         {
             if (fileName==audioClips[i].name)
                 SoundSystem.PlaySound(audioClips[i],gameObject);
+        }
+    }
+
+    public void PlayFootstep()
+    {   
+        //Set up layer mask to ignore character layer
+        int characterLayer = LayerMask.NameToLayer("Character");
+        LayerMask layerMask = ~(1 << characterLayer);
+
+        //Build Ray
+        Ray ray = new Ray(transform.position,Vector3.down);
+        RaycastHit hit;
+        Physics.Raycast(ray,out hit,Mathf.Infinity,layerMask,QueryTriggerInteraction.Ignore);
+
+        //Get tags
+        string[] tags = null;
+        if (TryGetComponent(out MultipleTagsTool multipleTagsTool))
+            tags = multipleTagsTool.GetTags();
+
+        //Check for matching tags
+        if (tags!=null)
+        {
+            for (int i = 0; i < footstepSounds.Length; i++)
+            {
+                if (tags.Contains(footstepSounds[i].tag))
+                {
+                    E_5_Character soundToPlay = footstepSounds[i].GetRandomSound();
+                    PlaySound(soundToPlay);
+                    return;
+                }    
+            }
+        }
+
+
+        //Play default footstepsounds in case not matching tag is found
+        for (int i = 0; i < footstepSounds.Length; i++)
+        {
+            if (footstepSounds[i].setAsDefault)
+            {
+                E_5_Character soundToPlay = footstepSounds[i].GetRandomSound();
+                PlaySound(soundToPlay);
+                return;
+            }    
         }
     }
 
