231dbd4fa4424a4f7bbc59b2f91c8d3429bb5ce6
Wolltoffel <lzandianpour@stud.macromedia.de>
2024-01-03 00:53:12 +0100

    Added Ambient Sound Player

---
+++ 2_UnityProject/Assets/1_Game/10_Sound/AmbientSoundPlayer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AmbientSoundPlayer : MonoBehaviour
+{
+    void PlayAmbientSound()
+    {
+
+    }
+}
