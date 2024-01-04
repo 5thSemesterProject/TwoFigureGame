72872b408a3e18972467cebfa9120299f432ca27
Wolltoffel <lzandianpour@stud.macromedia.de>
2024-01-04 13:29:50 +0100

    Fix Exclude Rectangle Usability Bug

--- 2_UnityProject/Assets/1_Game/2_Gas/Scripts/ExcludeRectangle.cs
+++ 2_UnityProject/Assets/1_Game/2_Gas/Scripts/ExcludeRectangle.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(BoxCollider))]
 public class ExcludeRectangle : MonoBehaviour
@@ -9,10 +10,16 @@ public class ExcludeRectangle : MonoBehaviour
     Vector3 scale;
     public Vector4 GetRectangleData()
     {
-        Vector2 rectangleCenter = VectorHelper.Convert3To2(transform.position);
-        scale = GetComponent<BoxCollider>().size;
-        float width = transform.localScale.x;
-        float height = transform.localScale.z;
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+        Vector2 rectangleCenter = VectorHelper.Convert3To2(transform.position+boxCollider.center);
+        Vector3 boxColliderSize = boxCollider.size;
+        scale = new Vector3(
+        boxColliderSize.x * transform.localScale.x,
+        boxColliderSize.y * transform.localScale.y,
+        boxColliderSize.z * transform.localScale.z
+        );
+        float width = scale.x;
+        float height = scale.z;
         return new Vector4(rectangleCenter.x,rectangleCenter.y,width,height);
     }
 }
